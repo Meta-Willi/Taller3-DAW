60d74336757bfc26db7c8cb64d4b225182d050f0
Meta-Willi <59988459+Meta-Willi@users.noreply.github.com>
2021-10-18 12:55:43 -0600

    Update peliculas.js

--- js/peliculas.js
+++ js/peliculas.js
@@ -36,7 +36,10 @@ function volcarDatos(datos){
     var total = datos.length;
     data = "<ul class=\"grid\">\n";
     for(var i=0; i<total; i++){
-        data += "<img src=\"" + datos[i].imagen + "\" alt=\"Portada " + datos[i].nombre + " \" class=\"avatar\" />\n";        
+        data += "<div class=\"imagen\">\n";
+        data += "<img src=\"" + datos[i].imagen + "\" alt=\"Portada " + datos[i].nombre + " \" class=\"avatar\" />\n";   
+
+        data += "</div>";
         data += "<li class=\"box\">\n";         
         data += "<div class=\"box-shadow\"></div>\n";
         data += "<div class=\"box-gradient\">\n";
